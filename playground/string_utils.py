f4d329d9d402309f11dc8e951d927ee6741fc34c
ThaTechMaestro <muse.pearls@gmail.com>
2024-09-15 18:40:54 +0100

    add faas:  text summarizer

---
+++ playground/string_utils.py
@@ -0,0 +1,6 @@
+occupation = "fishing"
+a = "This is what I do: {occupation}".format(occupation=occupation)
+print(a.format(occupation="fishing"))
+
+a = {"key": "1", "Link": "happy"}
+print(a.Link)
